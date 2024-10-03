84f8ca0ffb517c36e1cf2f00fd5ba785cfbfe757
ingta <ing.talero@gmail.com>
2024-10-02 22:00:04 -0500

    Backend

---
+++ neuromotion/src/main/java/com/neuromotion/neuromotion/NeuromotionApplication.java
@@ -0,0 +1,13 @@
+package com.neuromotion.neuromotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuromotionApplication.class, args);
+	}
+
+}
---
+++ neuromotion/src/test/java/com/neuromotion/neuromotion/NeuromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuromotion.neuromotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
